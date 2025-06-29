6876b301df6c8eb5c23d64e8d43f4af7f916e654
JactusTheCactus <threateninglyamusing@gmail.com>
2025-06-29 07:26:45 +0000

    -

--- script.py
+++ script.py
@@ -5,44 +5,45 @@ C: 0 - 600
 from defcon import Font
 import subprocess
 import json
+from fontTools.pens.transformPen import TransformPen
+def setWidth(g,l=80,r=90):
+	bounds = g.bounds
+	if bounds:
+		xMin, yMin, xMax, yMax = bounds
+		g.leftMargin = l
+		g.rightMargin = r
+		g.width = l + (xMax - xMin) + r
+	else:
+		g.leftMargin = l
+		g.rightMargin = r
+		g.width = l + r
 def toTuple(obj):
-    if isinstance(obj, list):
-        if len(obj) == 2 and all(isinstance(i, int) for i in obj):
-            return tuple(obj)
-        else:
-            return [toTuple(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {k: toTuple(v) for k, v in obj.items()}
-    else:
-        return obj
+	if isinstance(obj, list):
+		if len(obj) == 2 and all(isinstance(i, int) for i in obj):
+			return tuple(obj)
+		else:
+			return [toTuple(item) for item in obj]
+	elif isinstance(obj, dict):
+		return {k: toTuple(v) for k, v in obj.items()}
+	else:
+		return obj
 with open("data.json", "r") as f:
-    data = json.load(f)
+	data = json.load(f)
 glyphList = toTuple(data)
 font = Font()
-consonants = [
-	"b","c","d","ð","f",
-	"g","h","j","k","l",
-	"m","n","p","r","s",
-	"ś","t","v","w","x",
-	"y","z","ź","þ"
-]
-vowels = [
-	"","a","á","e","i",
-	"í","o","ó","u","ú"
-]
+with open("char.json", "r",encoding="utf-8") as f:
+	char = json.load(f)
+consonants, vowels = char["consonants"], char["vowels"]
 for g in glyphList:
 	glyph = font.newGlyph(g)
 	if g != ".notdef":
 		glyph.unicode = ord(g)
-	glyph.width = 700
 	pen = glyph.getPen()
 	for contour in glyphList[g]:
 		for i, pt in enumerate(contour):
-			if i == 0:
-				pen.moveTo(pt)
-			else:
-				pen.lineTo(pt)
+			pen.moveTo(pt) if i == 0 else pen.lineTo(pt)
 		pen.closePath()
+	setWidth(glyph)
 font.info.familyName = "Font"
 font.info.styleName = "Regular"
 font.info.fullName = f"{font.info.familyName} {font.info.styleName}"
@@ -55,9 +56,26 @@ font.info.capHeight = 1000
 font.info.xHeight = 500
 font.info.baseline = 0
 ufoName = f"{font.info.postscriptFontName}.ufo"
+for c in consonants:
+	if c in font:
+		for v in vowels:
+			if v in font:
+				if v:
+					lig_name = "_".join([c,v]) + ".liga"
+					if lig_name in font:
+						continue
+					lig = font.newGlyph(lig_name)
+					lig.clear()
+					c_glyph = font[c]
+					pen = lig.getPen()
+					c_glyph.draw(pen)
+					v_glyph = font[v]
+					pen = lig.getPen()
+					tpen = TransformPen(pen, (1, 0, 0, 1, 0, 0))
+					v_glyph.draw(tpen)
+					lig.unicode = None
+					setWidth(lig)
 font.save(ufoName)
-bash = f"fontmake -u {ufoName} -o otf".split()
-subprocess.run(bash, check=True)
 features_path = f"{ufoName}/features.fea"
 with open(features_path, "w", encoding="utf-8") as f:
 	f.write("feature liga {\n")
@@ -71,3 +89,12 @@ with open(features_path, "w", encoding="utf-8") as f:
 					lig_name = c
 					f.write(f"    sub {c} by {lig_name}.liga;\n")
 	f.write("} liga;\n")
+subprocess.run(
+	[
+		"fontmake",
+		"-u",
+		ufoName,
+		"-o",
+		"otf"
+	]
+)
