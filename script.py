ae10fd9cee20b7689a7e8b993ba232db2778310b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-03 05:21:53 +0000

    -

--- script.py
+++ script.py
@@ -6,13 +6,22 @@ import os
 with open("uni.json","r",encoding="utf-8") as f:
 	uni = json.load(f)
 def getUni(char):
+	"""
+Gets the unicode variant of `char`, following the format of;
+- `aacute` = &aacute;
+- `eth` = &eth;
+	"""
 	if char in uni:
 		out = uni.get(char)
 	else:
 		out = char
 	return out
 def setWidth(g,l=80,r=90):
+	"""
+Sets the width of a font glyph.
+	"""
 	bounds = g.bounds
+	#print(type(g))
 	if bounds:
 		xMin, yMin, xMax, yMax = bounds
 		g.leftMargin = l
@@ -23,15 +32,39 @@ def setWidth(g,l=80,r=90):
 		g.rightMargin = r
 		g.width = l + r
 def toTuple(obj):
+	"""
+	Takes an object and converts all child lists which only contain a pair of numbers into tuples.
+	e.g.;
+	```
+	[[20,80],[1,5]] ==> [(20,80),(1,5)]
+	```
+	"""
 	if isinstance(obj, list):
-		if len(obj) == 2 and all(isinstance(i, int) for i in obj):
-			return tuple(obj)
-		else:
-			return [toTuple(item) for item in obj]
+		return tuple(obj) if len(obj) == 2 and all(isinstance(i, int) for i in obj) else [toTuple(item) for item in obj]
 	elif isinstance(obj, dict):
 		return {k: toTuple(v) for k, v in obj.items()}
 	else:
 		return obj
+def cmdRun(args):
+	"""
+Runs commands exactly as written in the console.
+
+outputs;
+```
+cmdRun(args)["out"]
+cmdRun(args)["err"]
+```
+the output and errors of the console command
+	"""
+	arg = args.split()
+	log = subprocess.run(arg,capture_output=True,universal_newlines=True)
+	output = {
+		"out": log.stdout,
+		"err": log.stderr
+	}
+	return output
+line = "=" * 50
+print(f"\n{line}\n")
 with open("data.json", "r", encoding="utf-8") as f:
 	data = json.load(f)
 glyphList = toTuple(data)
@@ -67,62 +100,53 @@ font.info.xHeight = 500
 font.info.baseline = 0
 ufoName = f"{font.info.postscriptFontName}.ufo"
 def save():
+	"""
+Saves the font at the location defined in `ufoName`
+	"""
 	font.save(ufoName)
 if not os.path.exists(ufoName):
 	os.mkdir(ufoName)
+print("Successfully created ligatures for:\n")
 for c in consonants:
-	if c in font:
-		for v in vowels:
+	for v in vowels:
+		if c in font:
 			if v in font:
 				if v:
 					lig_name = "_".join([c,v]) + ".liga"
-					if lig_name in font:
-						continue
-					lig = font.newGlyph(lig_name)
-					lig.clear()
-					c_glyph = font[c]
-					pen = lig.getPen()
-					c_glyph.draw(pen)
-					v_glyph = font[v]
-					pen = lig.getPen()
-					tpen = TransformPen(pen, (1, 0, 0, 1, 0, 0))
-					v_glyph.draw(tpen)
-					lig.unicode = None
-					setWidth(lig)
+					if lig_name not in font:
+						lig = font.newGlyph(lig_name)
+						lig.clear()
+						c_glyph = font[c]
+						pen = lig.getPen()
+						c_glyph.draw(pen)
+						v_glyph = font[v]
+						pen = lig.getPen()
+						tpen = TransformPen(pen, (1, 0, 0, 1, 0, 0))
+						v_glyph.draw(tpen)
+						lig.unicode = None
+						setWidth(lig)
+	print(f"\t{getUni(c).upper() + getUni(c)}")
 fea = f"{ufoName}/features.fea"
 with open(fea, "w", encoding="utf-8") as f:
 	f.write("feature liga {\n")
 	for c in consonants:
-		c
 		for v in vowels:
 			if v:
-				v
 				lig_name = f"{c}_{v}"
 				f.write(f"    sub {c} {v} by {lig_name}.liga;\n")
 	f.write("} liga;\n")
 with open(fea) as f:
     feaText = f.read()
-#print(feaText)
 font.features.text = feaText  # Make sure it's set
 save()
 font = Font(ufoName)
-if 1:
-	line = "=" * 50
-	print("\n".join([
-		"",
-		line,
-		"",
-		subprocess.run(
-			[
-				"fontmake",
-				"-u",
-				ufoName,
-				"-o",
-				"otf"
-			],
-			capture_output = True,
-			universal_newlines = True,
-			check = True
-		).stderr,
-		line
-	]))
+output = cmdRun(f"fontmake -u {ufoName} -o otf")
+for i in ["out","err"]:
+	with open(f"logs/std{i}.md","w") as f:
+		content = (output[i] if output[i] else f"<STD{i.upper()}.null>").strip()
+		format = f"""
+```bash
+{content}
+```
+"""
+		f.write(format.strip())
