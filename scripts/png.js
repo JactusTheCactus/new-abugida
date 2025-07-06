27b8920a1b7ed2d860e94840df32edf83bece14b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-06 15:27:43 -0700

    Update png.js

--- scripts/png.js
+++ scripts/png.js
@@ -1,4 +1,4 @@
-const fs = require('fs-extra');
+import fs from 'fs-extra';
 const path = require('path');
 const { createCanvas } = require('canvas');
 const pdfjsLib = require('pdfjs-dist/legacy/build/pdf.js');
