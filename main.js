288f21f3700ce9b3ef75d99ad7c78c7f2bcd79e8
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-06 14:36:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import { spawn } from "child_process";
 import path from "path";
 const tasks = [
-	["python", ["script.py"]],
+	//["python", ["script.py"]],
 	["node", ["pdf.js"]],
-	["node", ["png.js"]],
+	//["node", ["png.js"]],
 ];
 function runTask(index = 0) {
 	if (index >= tasks.length) return;
