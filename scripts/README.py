8981248f0acdb16a1ec8d4f7631ecf096deb4e47
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-06 17:28:19 -0700

    Update README.py

--- scripts/README.py
+++ scripts/README.py
@@ -14,5 +14,9 @@ readmePath = os.path.join("README.md")
 with open(readmePath, "w", encoding = "utf-8") as f:
     f.write("")
 for i in list:
+    last = ""
     with open(readmePath, "a", encoding = "utf-8") as f:
-        f.write(f"\n# {i[1]}")
+        current = f"\n# {i[1]}"
+        if last != current:
+            f.write(current)
+            last = current
