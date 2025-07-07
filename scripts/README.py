1a332928ba5581f97469ca9e14cf3f0fd2477247
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-06 17:24:35 -0700

    Update README.py

--- scripts/README.py
+++ scripts/README.py
@@ -10,3 +10,9 @@ def listDirectories(basePath):
                 list.append([basePath, relativePath, f])
 listDirectories(os.path.join("site", "png"))
 print(list)
+readmePath = os.path.join("README.md")
+with open(readmePath, "w", encoding = "utf-8") as f:
+    f.write("")
+for i in list:
+    with open(readmePath, "a", encoding = "utf-8") as f:
+        f.write(f"\n# {i[1]}")
