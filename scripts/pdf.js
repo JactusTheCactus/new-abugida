6c509320509de13815f9b8b050fa41e33ffa1e5b
JactusTheCactus <threateninglyamusing@gmail.com>
2025-07-06 18:08:30 -0700

    Update pdf.js

--- scripts/pdf.js
+++ scripts/pdf.js
@@ -1,16 +1,6 @@
 import path from "path";
 import puppeteer from "puppeteer";
-
 (async () => {
-  function log(text) {
-    const line = "=".repeat(30);
-    console.log(`${line}\n${text}\n${line}`);
-  }
-
-  function percent(num) {
-    return Math.round((num + Number.EPSILON) * 100) + "%";
-  }
-
   const browser = await puppeteer.launch({
   args: [
 '--no-sandbox',
@@ -22,16 +12,11 @@ import puppeteer from "puppeteer";
     "https://jactusthecactus.github.io/new-abugida/site/html/ipa.html",
     "https://jactusthecactus.github.io/new-abugida/site/html/example.html"
   ];
-
-  log(`Generating ${pages.length} PDFs`);
-
-  let index = 1;
   for (const url of pages) {
     let pdf = url
       .replace(/.+\/(.*)/g, "$1.pdf")
       .replace(/\.html/g, "");
     pdf = pdf && pdf !== ".pdf" ? pdf : "index.pdf";
-
     const page = await browser.newPage();
     await page.goto(url, {
       waitUntil: "networkidle0"
@@ -44,11 +29,6 @@ import puppeteer from "puppeteer";
       preferCSSPageSize: true
     });
     await page.close();
-
-    console.log(`${pdf}; ${percent(index / pages.length)} Complete`);
-    index++;
   }
-
-  log(`${pages.length} PDFs Generated`);
   await browser.close();
 })();
